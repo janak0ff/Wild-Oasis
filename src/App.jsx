5f497fefcc8f2a58db631e3ee1c179a914b93aee
Janak kr Shrestha <janakkrshrestha@gmail.com>
2025-02-09 12:45:59 +0545

    Introduction to Styled Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,43 @@
+import styled from "styled-components";
+
 function App() {
+  const H1 = styled.h1`
+    color: red;
+    font-size: 100px;
+  `;
+
+  const Button = styled.button`
+    background-color: red;
+    color: white;
+    padding: 10px 20px;
+    border-radius: 5px;
+    border: none;
+    cursor: pointer;
+    margin: 10px;
+    padding: 10px 20px;
+  `;
+
+  const Input = styled.input`
+    padding: 10px 20px;
+    border-radius: 5px;
+    border: none;
+    cursor: pointer;
+  `;
+
+  const Container = styled.main`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  `;
+
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <Container>
+      <H1>Hello World</H1>
+      <Button onClick={() => alert("clicked 1")}>Click 1</Button>
+      <Button onClick={() => alert("clicked 2")}>Click 2</Button>
+      <Input type="text" placeholder="Enter your name" />
+    </Container>
   );
 }
 
