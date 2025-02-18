4d15ea92151009733c8ce098d84da33c941ecc75
Janak kr Shrestha <janakkrshrestha@gmail.com>
2025-02-18 16:41:36 +0545

    Converting the Modal to a Compound Component

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,24 +1,44 @@
-import { useState } from "react";
 import Button from "../../ui/Button";
 import Modal from "../../ui/Modal";
+import CabinTable from "./CabinTable";
 import CreateCabinForm from "./CreateCabinForm";
 
-const AddCabin = () => {
-  const [isOpenModel, setIsOpenModel] = useState(false);
-
+function AddCabin() {
   return (
-    <div>
-      <Button onClick={() => setIsOpenModel((show) => !show)}>
-        {isOpenModel ? "Close" : "Add cabin"}
-      </Button>
+    <Modal>
+      <Modal.Open opens="cabin-form">
+        <Button>Add new cabin</Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form">
+        <CreateCabinForm />
+      </Modal.Window>
 
-      {isOpenModel && (
-        <Modal onClose={() => setIsOpenModel(false)}>
-          <CreateCabinForm onCloseModal={() => setIsOpenModel(false)} />
-        </Modal>
-      )}
-    </div>
+      <Modal.Open opens="table">
+        <Button>Show Table</Button>
+      </Modal.Open>
+      <Modal.Window name="table">
+        <CabinTable />
+      </Modal.Window>
+    </Modal>
   );
-};
+}
+
+// const AddCabin = () => {
+//   const [isOpenModel, setIsOpenModel] = useState(false);
+
+//   return (
+//     <div>
+//       <Button onClick={() => setIsOpenModel((show) => !show)}>
+//         {isOpenModel ? "Close" : "Add cabin"}
+//       </Button>
+
+//       {isOpenModel && (
+//         <Modal onClose={() => setIsOpenModel(false)}>
+//           <CreateCabinForm onCloseModal={() => setIsOpenModel(false)} />
+//         </Modal>
+//       )}
+//     </div>
+//   );
+// };
 
 export default AddCabin;
--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,3 +1,4 @@
+import { cloneElement, createContext, useContext, useState } from "react";
 import styled from "styled-components";
 import { HiXMark } from "react-icons/hi2";
 import { createPortal } from "react-dom";
@@ -44,25 +45,104 @@ const Button = styled.button`
   & svg {
     width: 2.4rem;
     height: 2.4rem;
-    /* Sometimes we need both */
-    /* fill: var(--color-grey-500);
-    stroke: var(--color-grey-500); */
     color: var(--color-grey-500);
   }
 `;
 
-const Modal = ({ children, onClose }) => {
+const ModalContext = createContext();
+
+/**
+ * Modal component creates a context for managing modal windows.
+ * It provides the "open", "close" and "openName" state to its children.
+ *
+ * @param {Object} props - Component props
+ * @param {ReactNode} props.children - The children elements that will receive the context
+ * @returns {ReactElement} - A context provider that wraps the children elements
+ */
+function Modal({ children }) {
+  const [openName, setOpenName] = useState(""); // The name of the currently open modal window
+
+  /**
+   * Close the currently open modal window.
+   */
+  const close = () => setOpenName("");
+
+  /**
+   * Open a modal window with the given name.
+   * @param {string} name - The name of the modal window to open
+   */
+  const open = (name) => setOpenName(name);
+
+  return (
+    <ModalContext.Provider value={{ openName, close, open }}>
+      {children}
+    </ModalContext.Provider>
+  );
+}
+
+/**
+ * Open component wraps a clickable element and calls the `open` function from ModalContext
+ * when the wrapped element is clicked.
+ *
+ * @param {Object} props - Component props
+ * @param {ReactNode} props.children - Wrapped clickable element
+ * @param {string} props.opens - The name of the modal window to open when the wrapped element is clicked
+ * @returns {ReactElement} - The wrapped element with an onClick handler that calls the `open` function
+ */
+function Open({ children, opens: opensWindowName }) {
+  const { open } = useContext(ModalContext);
+
+  return cloneElement(children, {
+    onClick: (...args) => {
+      // Call the original onClick handler if it exists
+      children.props.onClick?.(...args);
+
+      // Open the modal window with the given name
+      open(opensWindowName);
+    },
+  });
+}
+
+/**
+ * Window component renders a modal window using React Portal.
+ * It displays the children content only when the window name matches the openName from context.
+ *
+ * @param {Object} props - Component props
+ * @param {ReactNode} props.children - The content to be displayed inside the modal
+ * @param {string} props.name - The name of the modal window
+ * @returns {ReactPortal|null} - Returns a portal with modal content or null if the window is not open
+ *
+ * The Window component is a controlled component that is connected to the ModalContext.
+ * It will only render the modal content if the name prop matches the openName from context.
+ * When the modal is open, the component will render a portal with the modal content.
+ * When the modal is closed, the component will render null.
+ *
+ * The component also provides a close function as the onCloseModal prop to the children element.
+ * This function can be used by the children element to close the modal window.
+ */
+function Window({ children, name }) {
+  const { openName, close } = useContext(ModalContext);
+
+  // If the name of this window does not match the open window name, return null
+  if (name !== openName) return null;
+
+  // Render the modal using React Portal
   return createPortal(
     <Overlay>
       <StyledModal>
-        <Button onClick={onClose}>
+        <Button onClick={close}>
           <HiXMark />
         </Button>
-        <div>{children}</div>
+
+        {/* Clone the children element and provide the close function as onCloseModal prop */}
+        <div>{cloneElement(children, { onCloseModal: close })}</div>
       </StyledModal>
     </Overlay>,
     document.body
   );
-};
+}
+
+Modal.Open = Open;
+Modal.Window = Window;
 
 export default Modal;
